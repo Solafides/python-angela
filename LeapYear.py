13db7bdc923c54667cdb666ae14dc95f5f095c76
Solomon <123379369+Solafides@users.noreply.github.com>
2024-12-07 09:46:22 +0300

    Calculates if it has a leap year or not

--- LeapYear.py
+++ LeapYear.py
@@ -1,1 +1,6 @@
-a=input("please enter the year to be checked if it is leap year or not \n")
+a=int(input("please enter the year to be checked if it is leap year or not \n"))
+if a%4==0 and a%100!=0 and a%400!=0:
+ print(f"{a} is a Leap year")
+else :
+ print(f"{a} is not a Leap year")
+    
