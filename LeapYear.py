4f045444cf0dc564cd6385e5382fc22e541828d5
Solomon <123379369+Solafides@users.noreply.github.com>
2024-12-07 10:56:07 +0300

    update leap year data

--- LeapYear.py
+++ LeapYear.py
@@ -1,6 +1,14 @@
 a=int(input("please enter the year to be checked if it is leap year or not \n"))
-if a%4==0 and a%100!=0 and a%400!=0:
- print(f"{a} is a Leap year")
-else :
- print(f"{a} is not a Leap year")
+if a%4==0:
+ if a%100==0:
+  if a%400==0:
+   print(f"{a} is a leap year")
+  else:
+   print(f"{a} Is not a leap year")
+ else:
+  print(f"{a} is a leap year")
+   
+ 
+
+    
     
